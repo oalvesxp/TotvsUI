c655201862306f7614c2142c25378402c6d128bd
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-20 15:15:36 -0300

    Fonte primária Global

--- TotvsUI/pages/_document.tsx
+++ TotvsUI/pages/_document.tsx
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { lato } from '../app/ui/fonts'
 
 export default function Document() {
   return (
     <>
       <Html lang="pt-br">
         <Head />
-        <body>
+        <body className={`${lato.className} antialiased`}>
           <Main />
           <NextScript />
         </body>
--- TotvsUI/pages/index.tsx
+++ TotvsUI/pages/index.tsx
@@ -1,10 +1,10 @@
-import { inter, lato } from '../app/ui/fonts'
+import { inter } from '../app/ui/fonts'
 
 export default function Page() {
   return (
     <>
       <h1 className={`${inter.className} antialiased`}>Hello World!</h1>
-      <p className={`${lato.className} antialiased`}>
+      <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
         eum nobis dignissimos!
       </p>
