b2120de8868730c9f735b332968f3d019043dde6
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-20 14:26:11 -0300

    Fontes

--- TotvsUI/pages/index.tsx
+++ TotvsUI/pages/index.tsx
@@ -1,7 +1,13 @@
+import { inter, lato } from '../app/ui/fonts'
+
 export default function Page() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 className={`${inter.className} antialiased`}>Hello World!</h1>
+      <p className={`${lato.className} antialiased`}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+        eum nobis dignissimos!
+      </p>
     </>
   )
 }
