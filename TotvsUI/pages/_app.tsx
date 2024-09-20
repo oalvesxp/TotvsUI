0b021fa01fbd00682407291953d5ee3e0bfdb7f9
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-20 15:53:36 -0300

    Reset CSS

--- TotvsUI/pages/_app.tsx
+++ TotvsUI/pages/_app.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Layout from '../app/layout'
+import ResetCSS from '../src/theme/ResetCSS'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Layout>
+        <ResetCSS />
         <Component {...pageProps} />
       </Layout>
     </>
