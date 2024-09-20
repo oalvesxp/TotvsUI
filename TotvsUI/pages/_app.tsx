c09bd4564e5408820fd2ac7f6e1c2647d9a2c6f9
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-20 14:24:05 -0300

    charset utf-8 como padrão

--- TotvsUI/pages/_app.tsx
+++ TotvsUI/pages/_app.tsx
@@ -4,6 +4,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
