c27b1fbc98ea661e9a8a09c61d34103dfa0e87de
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-24 11:19:56 -0300

    Página de Login (modelo)

--- TotvsUI/app/layout.tsx
+++ TotvsUI/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
