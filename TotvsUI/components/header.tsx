97eca2c541bfcb78bc3600c1da15b955e1c9452f
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-25 16:23:14 -0300

    Botão de login

--- TotvsUI/components/header.tsx
+++ TotvsUI/components/header.tsx
@@ -16,27 +16,29 @@ const Header = () => {
             <Link href="/private">Privada</Link>
           </li>
           <li>
-            <button
-              type="button"
-              className="text-white bg-[#0089cc] hover:bg-[#006799] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Logout
-              <svg
-                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
+            <Link href="/auth/login">
+              <button
+                type="button"
+                className="text-white bg-[#0089cc] hover:bg-[#006799] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </button>
+                Login
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </button>
+            </Link>
           </li>
         </ul>
       </nav>
