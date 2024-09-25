a70ee9d4d2b47fd7b378b356c3aad3b0fe92f678
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-25 11:26:22 -0300

    Criando página Pública e Privada para testes de autenticação

---
+++ TotvsUI/app/private/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivatePage() {
+  return (
+    <>
+      <h1 className="font-semibold text-lg">Private Page</h1>
+    </>
+  )
+}
---
+++ TotvsUI/app/public/page.tsx
@@ -0,0 +1,7 @@
+export default function PublicPage() {
+  return (
+    <>
+      <h1 className="font-semibold text-lg">Public Page</h1>
+    </>
+  )
+}
