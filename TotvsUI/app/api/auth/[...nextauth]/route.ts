f4b4a779d080c076c42d04c67108353b44a99b8c
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-25 16:17:30 -0300

    Auth handler

--- TotvsUI/app/api/auth/[...nextauth]/route.ts
+++ TotvsUI/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,40 @@
 import NextAuth from 'next-auth/next'
-import { NextAuthOptions } from 'next-auth'
+import CredentialProvider from 'next-auth/providers/credentials'
 
-const authOptions: NextAuthOptions = {
-  providers: [],
-}
+const handler = NextAuth({
+  providers: [
+    CredentialProvider({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Insira seu usuário',
+          type: 'text',
+          placeholder: 'Ex: sp01\\nome.sobrenome',
+        },
+        password: { label: 'Insira sua senha', type: 'password' },
+      },
+      async authorize(credentials) {
+        const username = credentials?.username
+        const password = credentials?.password
 
-const handler = NextAuth(authOptions)
+        const res = await fetch(
+          `${process.env.TOTVS_API_URL}/api/oauth2/v1/token?grant_type=password&username=${username}&password=${password}`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=utf-8',
+            },
+          }
+        )
+
+        const user = await res.json()
+        if (!res.ok && !user) return null
+
+        return user
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+})
 
 export { handler as GET, handler as POST }
