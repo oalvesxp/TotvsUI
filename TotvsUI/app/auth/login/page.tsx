1652cf3f6e87f79906628c9cf8132030e34b496a
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-26 10:59:34 -0300

    AutoComplete para o campo de username

--- TotvsUI/app/auth/login/page.tsx
+++ TotvsUI/app/auth/login/page.tsx
@@ -60,6 +60,7 @@ export default function Login() {
                     name="username"
                     type="text"
                     placeholder="Digite seu usuário"
+                    autoComplete="username"
                     required
                     className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#0089cc] sm:text-sm sm:leading-6"
                   />
