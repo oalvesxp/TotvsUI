75cd40048d3056bf5a301012be1997c19953c0b2
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-27 13:51:43 -0300

    Ajustanto o Layout do <pre> de session

--- TotvsUI/app/public/page.tsx
+++ TotvsUI/app/public/page.tsx
@@ -7,10 +7,13 @@ export default function PublicPage() {
 
   return (
     <>
-      <h1 className="font-semibold text-lg">Public Page</h1>
-      <pre className="bg-slate-900 text-slate-50 p-10 rounded-lg mt-10">
-        {JSON.stringify(session, null, 2)}
-      </pre>
+      <div className="max-w-full content-center px-[100px]">
+        <h1 className="font-semibold text-lg text-center">Public Page</h1>
+
+        <pre className="bg-slate-900 text-slate-50 p-5 sm:p-8 lg:p-10 rounded-lg mt-10  overflow-x-auto break-words whitespace-pre-wrap">
+          {JSON.stringify(session, null, 2)}
+        </pre>
+      </div>
     </>
   )
 }
