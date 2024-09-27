9ea8cbaa9315cf0a516ba3f43f19a04707b7b169
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-09-27 07:53:55 -0300

    Configurando o Route para fazer o fetch dos dados do usuário e salvar os tokens da Totvs nos cookies

--- TotvsUI/app/api/auth/[...nextauth]/route.ts
+++ TotvsUI/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth/next'
 import CredentialProvider from 'next-auth/providers/credentials'
+import { TokenSVC } from './tokenSVC'
+import { UserSVC } from './userSVC'
+import { NextResponse } from 'next/server'
 
 export const authOptions = {
   providers: [
@@ -14,26 +17,64 @@ export const authOptions = {
         password: { label: 'Insira sua senha', type: 'password' },
       },
       async authorize(credentials) {
-        const username = credentials?.username
-        const password = credentials?.password
-
-        const res = await fetch(
-          `${process.env.TOTVS_API_URL}/api/oauth2/v1/token?grant_type=password&username=${username}&password=${password}`,
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json;charset=utf-8',
-            },
-          }
+        /** Autenticando o usuário */
+        const res = await TokenSVC.get(
+          `${process.env.TOTVS_API_URL}/api/oauth2/v1/token`,
+          credentials?.username,
+          credentials?.password
         )
 
-        const user = await res.json()
-        if (!res.ok && user.code !== 201) return null
+        console.log(res)
 
-        return user
+        const tokens = await res.json()
+        if (!res.ok && tokens.code !== 201) return null
+
+        /** Salvando os Tokens do usuário nos Cookies */
+        TokenSVC.save(NextResponse, tokens.access_token, tokens.refresh_token)
+
+        const data = await UserSVC(
+          `${process.env.TOTVS_API_URL}/api/framework/v1/users/`,
+          tokens.access_token
+        )
+
+        return {
+          id: data.items.id,
+          username: data.items.userName,
+          name: data.items.displayName,
+          department: data.items.department,
+          active: data.items.active,
+          access_token: tokens.access_token,
+          refresh_token: tokens.refresh_token,
+        }
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }: any) {
+      if (user) {
+        token.id = user.id
+        token.username = user.username
+        token.name = user.name
+        token.department = user.department
+        token.active = user.active
+        token.access_token = user.access_token
+        token.refresh_token = user.refresh_token
+      }
+
+      return token
+    },
+    async session({ session, token }: any) {
+      session.user.id = token.id
+      session.user.username = token.username
+      session.user.name = token.name
+      session.user.department = token.department
+      session.user.active = token.active
+      session.user.access_token = token.access_token
+      session.user.refresh_token - token.refresh_token
+
+      return session
+    },
+  },
   pages: {
     signIn: '/auth/login',
   },
